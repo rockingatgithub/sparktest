1db5d329eaefb7374e3a64c51dc3119f99121d90
rockingatgithub <contactwithsid@gmail.com>
2020-10-17 13:54:27 +0530

    test files added

---
+++ __test__/App.spec.js
@@ -0,0 +1,11 @@
+import { App } from "../src/components/App";
+import configureStore from "redux-mock-store";
+import renderer from "react-test-renderer";
+
+// Snapshot for Home React Component
+describe(">>>H O M E --- Snapshot", () => {
+  it("+++capturing Snapshot of Home", () => {
+    const renderedValue = renderer.create(<App />).toJSON();
+    expect(renderedValue).toMatchSnapshot();
+  });
+});
